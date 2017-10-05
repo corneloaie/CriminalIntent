f6fdab3f27ce6c39eb7212f06b94d716d015fb5f
corneloaie <corneloaie@gmail.com>
2017-10-05 18:58:24 +0300

    CriminalIntent app
    just a tutorial

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Cornel-PC on 05/10/2017.
+ */
+
+public class CrimeListFragment extends Fragment {
+}
